5d276704b5268443369cd4829a48c963af24a2d6
Stephen Johnson-Tseu <stephjohnsons@me.com>
2023-02-06 23:23:41 +0800

    modified

--- script/script.js
+++ script/script.js
@@ -3,22 +3,40 @@ function greetUser() {
     const hour = date.getHours(); 
     let greetMessage;
 
+    let editable = "<span contenteditable> you</span>"
+
     if (hour < 12 ) {
-        greetMessage = "good morning, you."
+        greetMessage = `good morning, ${editable}.`
     } else if (hour < 18) {
-        greetMessage = "good afternoon, you."
-    } else if (hour < 23) {
-        greetMessage = "good evening, you."
+        greetMessage = `good afternoon, ${editable}.`
     } else {
-        greetMessage = "it's late. good evening, you."
+        greetMessage = `good evening, ${editable}.`
     };
 
     document.getElementById("greeting").innerHTML = greetMessage;
 }
 
-window.addEventListener("scroll", function moveWithScroll() {
-    message.style.top = window.pageYOffset + "px",
-    message.style.left = window.pageXOffset + "px"
-});
+function jobTitle() {
+    const job1 = "web developer";
+    const job2 = "web designer";
+    const job3 = "digital content creator";
+
+    let currentJob = job1; 
+
+    const span = document.querySelector(".job");
+    span.textContent = currentJob;
+
+    span.addEventListener('click', function() {
+    if (currentJob === job1) {
+        currentJob = job2;
+    } else if (currentJob === job2) {
+        currentJob = job3;
+    } else {
+        currentJob = job1;
+    }
+    span.textContent = currentJob;
+    });
+};
 
 window.onload = greetUser();
+window.onload = jobTitle();
